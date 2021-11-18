a6886b4b8ab0e59e410215e04c921d77f588c315
suzyc <suzy.cusin@gmail.com>
2021-11-18 17:34:30 +0100

    Création de la page login

---
+++ src/pages/loginPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-page class="bg-primary row justify-center items-center">
+
+    <div class="column">
+      <div class="row justify-center">
+        <img src="../assets/Logo1.png" style="width:30%;height:30%;"/>
+      </div>
+
+      <div class="row justify-center">
+        <h5 class="text-h5 text-white q-my-md">Page de connexion</h5>
+      </div>
+      <div class="row justify-center">
+        <q-card class="my-card">
+          <q-card-section>
+            <q-form class="q-gutter-md">
+              <q-input square filled clearable v-model="email" type="email" label="email" />
+              <q-input square filled clearable v-model="password" type="password" label="password" />
+            </q-form>
+          </q-card-section>
+          <q-card-actions class="q-px-md">
+            <q-btn unelevated to="/" color="primary" size="lg" class="full-width" label="Se connecter" />
+          </q-card-actions>
+          <q-card-section class="text-center q-pa-none">
+            <p class="text-grey-6">Not registered? Created an Account</p>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  components:{
+
+  },
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
