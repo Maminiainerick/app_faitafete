a212f0571fc94c8b8d6e4279c7663ae0d294f4df
suzyc <suzy.cusin@gmail.com>
2021-11-19 15:48:44 +0100

    Création de la page login

--- src/pages/signUpPage.vue
+++ src/pages/signUpPage.vue
@@ -7,12 +7,13 @@
       </div>
 
       <div class="row justify-center">
-        <h5 class="text-h5 text-white q-my-md">Page de connexion</h5>
+        <h5 class="text-h5 text-white q-my-md">Page d'inscription</h5>
       </div>
       <div class="row justify-center">
         <q-card class="my-card">
           <q-card-section>
             <q-form class="q-gutter-md">
+              <q-input square filled clearable v-model="login" type="login" label="pseudo" />
               <q-input square filled clearable v-model="email" type="email" label="email" />
               <q-input square filled clearable v-model="password" type="password" label="password" />
             </q-form>
@@ -32,12 +33,13 @@
 <script>
 
 export default {
-  name: 'Login',
+  name: 'signUp',
   components:{
 
   },
   data () {
     return {
+      login:'',
       email: '',
       password: ''
     }
