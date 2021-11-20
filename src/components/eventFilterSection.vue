8e386faba8e69a7b88068dd77f2bb86ddd046f33
Erick Maminiaina <erick.maminiaina@outlook.fr>
2021-11-20 03:35:46 +0100

    Redéfinition des pages d'accueil pour interface standard et interface connectée

---
+++ src/components/eventFilterSection.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="text-h6">Découvrir des événements</div>
+    <q-chip
+      v-model:selected="event.Recent"
+      color="primary"
+      text-color="white"
+      icon="eva-clock-outline"
+      size="sm"
+    >
+      Ajoutés récemment
+    </q-chip>
+    <q-chip
+      v-model:selected="event.Semaine"
+      color="primary"
+      text-color="white"
+      icon="eva-calendar-outline"
+      size="sm"
+    >
+      Cette semaine
+    </q-chip>
+    <q-chip
+      v-model:selected="event.Proximite"
+      color="primary"
+      text-color="white"
+      icon="eva-pin-outline"
+      size="sm"
+    >
+      A proximité
+    </q-chip>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  setup () {
+    return {}
+  }
+}
+</script>
---
+++ src/components/greetingSectionCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-card>
+    <q-card-section>
+      <div class="text-h5">
+        Bonjour,<br>Johann
+      </div>
+    </q-card-section>
+    <q-card-section>
+      <option-section-card/>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import OptionSectionCard from "components/optionSectionCard";
+export default {
+  components: {OptionSectionCard},
+  // name: 'ComponentName',
+  setup () {
+    return {}
+  }
+}
+</script>
