9811c33e8c27237d2f1e1cc05e7ebfea49be9a45
Erick Maminiaina <erick.maminiaina@outlook.fr>
2021-11-20 03:37:08 +0100

    Création de deux layouts Layout pour affichage standard Layout pour affichage connecté

---
+++ src/layouts/MainLoggedInLayout.vue
@@ -0,0 +1,78 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title class="row justify-center">
+          <logo-img to="/loggedin/index"/>
+        </q-toolbar-title>
+
+        <div>
+          <q-btn
+            round
+            to="/loggedin/profile"
+          >
+            <q-avatar size="42px">
+              <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+            </q-avatar>
+          </q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+          <div class="text-weight-bold">Razvan Stoenescu</div>
+          <div>@rstoenescu</div>
+        </div>
+      </q-img>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+import { defineComponent, ref } from 'vue'
+import LogoImg from "components/logoImg";
+import AvatarButton from "components/avatarButton";
+import EventButton from "components/eventButton";
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    LogoImg
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ src/layouts/MainStandardLayout.vue
@@ -0,0 +1,96 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title class="row justify-center">
+          <logo-img/>
+        </q-toolbar-title>
+
+        <div>
+          <q-btn
+            round color="primary"
+            icon="eva-person-outline"
+            to="login"
+          />
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-img
+        class="absolute-top"
+        style="height: 150px">
+        <div class="absolute-bottom bg-blue-1">
+          <q-btn
+            round
+            flat
+            color="primary"
+            icon="eva-person-outline"
+            to="login"
+          />
+          <div
+            class="text-weight-bold text-primary"
+          >
+            Se connecter
+          </div>
+          <div
+            class="text-info text-black text-weight-regular">
+            Pas encore de compte ?
+          </div>
+          <div
+            class="text-info
+                   text-black
+                   text-weight-regular
+                   text-italic"
+            >
+            Inscrivez-vous
+          </div>
+        </div>
+      </q-img>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+import { defineComponent, ref } from 'vue'
+import LogoImg from "components/logoImg";
+import AvatarButton from "components/avatarButton";
+import EventButton from "components/eventButton";
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    LogoImg
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
