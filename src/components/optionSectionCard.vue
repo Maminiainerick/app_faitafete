676f2cca0bacd976998731c0da18f149b4d7a977
Erick Maminiaina <erick.maminiaina@outlook.fr>
2021-11-20 03:41:15 +0100

    Section d'option sur la page générale (pas encore optimale)

---
+++ src/components/optionSectionCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <q-btn
+      flat
+      round
+      icon="eva-more-horizontal-outline"
+    />
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  setup () {
+    return {}
+  }
+}
+</script>
