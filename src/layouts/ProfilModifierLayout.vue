e8e21d03875d7038811006d35c042f60d9cc00e2
suzyc <suzy.cusin@gmail.com>
2021-12-03 17:02:42 +0100

    Modification du Layout de la page profil modifier

--- src/layouts/ProfilModifierLayout.vue
+++ src/layouts/ProfilModifierLayout.vue
@@ -30,29 +30,84 @@
 
     <q-drawer
       v-model="leftDrawerOpen"
+      id="drawer"
       show-if-above
+      :width="200"
+      :breakpoint="500"
       bordered
+      class="bg-grey-3"
     >
-      <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
-        <div class="absolute-bottom bg-transparent">
-          <q-avatar size="56px" class="q-mb-sm">
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-          </q-avatar>
-          <div class="text-weight-bold">Razvan Stoenescu</div>
-          <div>@rstoenescu</div>
-        </div>
-      </q-img>
-    </q-drawer>
+      <q-card class="my-card">
+        <q-card-section>
+          <div class="q-pa-md">
+            <div class="column" style="height: 150px">
+              <div class="col-4">
+                <div>
+                  <q-btn
+                    flat
+                    round
+                    v-model="leftDrawerOpen"
+                    size="10px"
+                  >
+                    <q-avatar size="20px" icon="close">
 
+                    </q-avatar>
+                  </q-btn>
+                </div>
+              </div>
+              <div class="col-4">
+                <div>
+                  <q-btn
+                    round
+                    to="/loggedin/profile"
+                  >
+                    <q-avatar size="50px">
+                      <img src="https://cdn-s-www.estrepublicain.fr/images/BFFFE250-24DD-44A1-B140-6A9229B524FB/NW_raw/photo-dr-1573744884.jpg">
+                    </q-avatar>
+                  </q-btn>
+                </div>
+              </div>
+              <div class="col-4">
+                <div class=" text-h6 text-bold">
+                  Pascal
+                </div>
+                <div class="text-info">
+                  <q-item clickable to="/" dense>
+                    Se déconnecter
+                  </q-item>
+                </div>
+              </div>
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+      <q-scroll-area class="fit">
+        <q-list>
+
+          <template v-for="(menuItem, index) in menuList" :key="index">
+            <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+              <q-item-section avatar>
+                <q-icon :name="menuItem.icon"/>
+              </q-item-section>
+              <q-item-section>
+                {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+            <q-separator :key="'sep' + index" v-if="menuItem.separator"/>
+          </template>
+
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
     <q-page-container>
-      <router-view />
+      <router-view/>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
 
-import { defineComponent, ref } from 'vue'
+import {defineComponent, ref} from 'vue'
 import LogoImg from "components/logoImg";
 import AvatarButton from "components/avatarButton";
 import EventButton from "components/eventButton";
@@ -64,15 +119,44 @@ export default defineComponent({
     LogoImg
   },
 
-  setup () {
+  setup() {
     const leftDrawerOpen = ref(false)
+    const menuList = [
+      {
+        icon: 'calendar_today',
+        label: 'Mes événements',
+        separator: true
+      },
+      {
+        icon: 'view_agenda',
+        label: 'Mon agenda',
+        separator: false
+      },
+      {
+        icon: 'message',
+        label: 'Ma messagerie',
+        separator: false
+      },
+      {
+        icon: 'settings',
+        label: 'Paramètres',
+        separator: true
+      }
+    ]
 
     return {
       leftDrawerOpen,
-      toggleLeftDrawer () {
+      toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      },
+      menuList
     }
   }
 })
 </script>
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  height: 200px
+  max-width: 250px
+</style>
