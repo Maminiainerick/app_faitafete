7deb21ad28830529e8c7f4a25a08de0b6b5012da
suzyc <suzy.cusin@gmail.com>
2021-12-03 16:51:55 +0100

    Modification de la page Profil !

--- src/pages/ModificationProfilePage.vue
+++ src/pages/ModificationProfilePage.vue
@@ -1,16 +1,111 @@
 <template>
-  <q-page padding>
-    changement pseudo
-    changement mot de passe
-    changement adresse mail
-    changement numéro de téléphone
-    changement description
-    changement image de profil
+  <q-page>
+    <q-card class="my-card" flat>
+      <q-item class="justify-center">
+          <q-uploader
+            url="http://localhost:4444/upload"
+            label="Photo de profil"
+            color="primary"
+            style="max-width: 70%"
+          />
+      </q-item>
+    </q-card>
+        <q-item-section>
+          <div class="text-h4">
+            <q-input
+              label="Nom"
+              v-model="name"
+              class="text-h4"
+            />
+          </div>
+          <q-input
+            autogrow
+            label="Description"
+            v-model="description"
+            class="text-italic">
+          </q-input>
+        </q-item-section>
+
+
+    <div class="q-pa-md items-start">
+      <q-card style="background: rgba(111,145,181,0.32)">
+        <q-item>
+          <q-item-section>
+            <div class="text-subtitle1">
+              Contact :
+            </div>
+            <div class="row">
+              <div class="col-10">
+                <div class="text-info text-black">
+                  <q-input
+                    borderless
+                    v-model="phone"
+                  >
+                    <template v-slot:prepend>
+                      <q-icon size="xs" name="eva-phone"/>
+                    </template>
+                  </q-input>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-10">
+                <div class="text-info text-black">
+                  <q-input
+                    borderless
+                    v-model="email">
+                    <template v-slot:prepend>
+                      <q-icon name="eva-at"/>
+                    </template>
+                  </q-input>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-10">
+                <div class="text-info text-black">
+                  <q-input
+                    borderless
+                    type="password"
+                    v-model="mdp">
+                    <template v-slot:prepend>
+                      <q-icon name="eva-at"/>
+                    </template>
+                  </q-input>
+                </div>
+              </div>
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-card>
+    </div>
+
+    <q-item class="justify-center">
+      <q-btn
+        class ="justify-center"
+        unelevated rounded
+        color="dark"
+        label="Enregistrer les modifications"
+        to="/modifier"
+      />
+    </q-item>
+
   </q-page>
 </template>
 
 <script>
+import {ref} from 'vue'
 export default {
   // name: 'PageName',
+  setup(){
+    return {
+      name: ref('Johann'),
+      description:ref('"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."'),
+      phone : ref ('01 02 03 04 05'),
+      email: ref ('johann@gmail.com'),
+      mdp: ref('Johannlesang')
+    }
+  }
 }
 </script>
---
+++ src/pages/ProfilModifierPage.vue
@@ -0,0 +1,164 @@
+<template>
+  <q-page>
+    <q-card class="my-card" flat>
+      <q-item>
+        <q-item-section avatar>
+          <q-btn round>
+            <q-avatar size="100px">
+              <img src="https://cdn-s-www.estrepublicain.fr/images/BFFFE250-24DD-44A1-B140-6A9229B524FB/NW_raw/photo-dr-1573744884.jpg">
+            </q-avatar>
+          </q-btn>
+        </q-item-section>
+
+        <q-item-section>
+          <div class="text-h4">
+            Pascal
+          </div>
+          <q-item-label caption class="text-italic">Bonjour je suis Pascal. Hâte de faire des soirées avec vous!
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-card>
+    <div class="q-pa-md items-start">
+      <q-card style="background: rgba(111,145,181,0.32)">
+        <q-item>
+          <q-item-section>
+            <div class="text-subtitle1">
+              Contact :
+            </div>
+            <div class="row">
+              <div class="col-1">
+                <q-icon name="eva-phone"/>
+              </div>
+              <div class="col-10">
+                <div class="text-info text-black">
+                  06 06 06 06 06
+                </div>
+              </div>
+              <div class="col-1">
+                <q-icon name="eva-lock" class="disabled"/>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-1">
+                <q-icon name="eva-at"/>
+              </div>
+              <div class="col-10">
+                <div class="text-info text-black">
+                  pascallegrandfrere@gmail.com
+                </div>
+              </div>
+              <div class="col-1">
+                <q-icon name="eva-lock" class="disabled"/>
+              </div>
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-card>
+    </div>
+    <div class="q-pa-md items-start q-gutter-md">
+      <q-card style="background: rgba(239,143,136,0.29)">
+        <q-item clickable to="/loggedin/myagenda">
+          <q-item-section>
+            <div class="row">
+              <div class="col-1">
+                <q-icon name="eva-calendar"/>
+              </div>
+              <div class="col-11">
+                <div class="text-info text-black">
+                  Mon agenda
+                </div>
+              </div>
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-card>
+      <q-card style="background: rgba(111,145,181,0.32)">
+        <q-item clickable to="/loggedin/myevents">
+          <q-item-section>
+            <div class="row">
+              <div class="col-1">
+                <q-icon name="eva-monitor"/>
+              </div>
+              <div class="col-11">
+                <div class="text-info text-black">
+                  Mes événements
+                </div>
+              </div>
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-card>
+      <q-card style="background: rgba(239,143,136,0.29)">
+        <q-item clickable to="/message">
+          <q-item-section>
+            <div class="row">
+              <div class="col-1">
+                <q-icon name="eva-message-square"/>
+              </div>
+              <div class="col-11">
+                <div class="text-info text-black">
+                  Ma messagerie
+                </div>
+              </div>
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-card>
+    </div>
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn color="primary" flat rounded label="Modifier le profil" to="/loggedin/modifprofile"/>
+      <q-btn color="secondary" flat rounded label="Se déconnecter" to="/" @click="showNotificationDisconnect"/>
+      <q-btn color="red-3" flat rounded label="Supprimer le profil" @click="customBtn"/>
+    </div>
+
+  </q-page>
+</template>
+
+<script>
+import {useQuasar} from "quasar";
+
+export default {
+  // name: 'PageName',
+
+  setup() {
+
+    const $q = useQuasar()
+
+    return {
+      showNotificationDisconnect: function () {
+        $q.notify({
+          progress: true,
+          message: 'Vous êtes déconnecté !',
+          color: 'primary'
+        })
+      },
+      customBtn: function () {
+        $q.dialog({
+          title: 'Supprimer le profil',
+          message: 'Vous êtes sûr(e) de vouloir supprimer votre profil ?',
+          ok: {
+            push: true,
+            color: 'negative',
+            label: 'Oui, supprimer mon profil',
+            onclick: "document.location.href=\'http://localhost:8080/#/'"
+          },
+          cancel: {
+            push: true,
+            color: 'primary',
+            label: 'Annuler'
+          },
+          persistent: true
+        }).onOk(() => {
+
+          // console.log('>>>> OK')
+        }).onCancel(() => {
+          // console.log('>>>> Cancel')
+        }).onDismiss(() => {
+          // console.log('I am triggered on both OK and Cancel')
+        })
+      }
+    }
+  }
+}
+</script>
