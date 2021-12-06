bfb2468ac213a97ed5822d8ac79e0966accb3fb9
suzyc <suzy.cusin@gmail.com>
2021-12-06 14:04:18 +0100

    Modification de l'évènement

--- src/pages/ListMyEventsPage.vue
+++ src/pages/ListMyEventsPage.vue
@@ -24,7 +24,7 @@
         </div>
       </q-card-section>
       <q-card-actions>
-        <q-btn push size="sm" padding="sm md" rounded color="primary" label="Modifier"/>
+        <q-btn push size="sm" padding="sm md" rounded color="primary" label="Modifier" to="/loggedin/modification-profil"/>
         <q-btn rounded size="sm" padding="sm md" outline color="dark" label="Supprimer" @click="supprBtn"/>
       </q-card-actions>
     </q-card>
---
+++ src/pages/ModifyEventPage.vue
@@ -0,0 +1,115 @@
+<template>
+  <q-page padding>
+    <div class="text-h5 text-primary text-bold text-center">Modifier un événement</div>
+    <div class="q-pa-md">
+    <q-form @submit="onSubmit" @reset="onReset">
+        <q-input
+          v-model="titre"
+          label="Titre de l'événement"
+          counter
+          maxlength="50"
+          :rules="[val => !!val || 'Champ obligatoire']"/>
+        <q-input
+          v-model="date"
+          type="date"
+          :rules="[val => !!val || 'Champ obligatoire']"/>
+        <q-input
+          v-model="lieu"
+          label="Lieu"
+          :rules="[val => !!val || 'Champ obligatoire']"/>
+        <q-input
+          v-model="description"
+          autogrow
+          label="Description"
+          counter
+          maxlength="250"
+          :rules="[val => !!val || 'Champ obligatoire']"/>
+        <div class="text-info">
+          Confidentialité
+        </div>
+        <q-radio
+          v-model="shape"
+          val="privé"
+          label="En petit comité"/>
+        <q-radio
+          v-model="shape"
+          val="Public"
+          label="Ouvert à tous"/>
+        <q-input
+          v-model="nbParticipants"
+          type="number"
+          label="Nombre de participants"
+          counter
+          maxlength="3"/>
+        <div class="text-info">Autres options</div>
+        <div class="bg-grey-2 q-pa-sm rounded-borders">
+          <q-checkbox
+            v-model="alcool"
+            true-value="alcool"
+            label="Alcool"
+          />
+          <q-checkbox
+            v-model="deguisement"
+            true-value="déguisé"
+            label="Déguisements"
+          />
+          <q-checkbox
+            v-model="accompagnant"
+            true-value="accompagnant"
+            label="Accompagnant autorisé"
+          />
+        </div>
+        <q-card flat>
+          <q-card-section>
+            <div class="text-center">
+              <q-btn label="Créer" type="submit" color="primary" @click="onSubmit" to="/loggedin/myevents"/>
+              <q-btn label="Annuler" type="reset" color="primary" flat class="q-ml-sm" @click="onReset"
+                     to="/loggedin/profile"/>
+            </div>
+          </q-card-section>
+        </q-card>
+
+      </q-form>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import {useQuasar} from "quasar";
+import {ref} from "vue";
+
+export default {
+  setup() {
+    const $q = useQuasar()
+    return {
+      titre: ref('Anniversaire Johann'),
+      date: ref('12/12/2021'),
+      lieu: ref('Bar à jeu'),
+      theme: ref(''),
+      nbParticipants: ref('12'),
+      description: ref(''),
+      shape: ref(''),
+      alcool: ref('null'),
+      deguisement: ref('null'),
+      accompagnant: ref('null'),
+
+      onSubmit() {
+        $q.notify({
+          color: 'primary',
+          textColor: 'white',
+          icon: 'checked',
+          message: 'Evénement créé !'
+        })
+      },
+
+      onReset() {
+        $q.notify({
+          color: 'grey',
+          textColor: 'white',
+          message: 'Annulé'
+        })
+      }
+    }
+  }
+}
+</script>
